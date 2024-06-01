125a58f771dba341fb10a5b2a45ae4d136fa8ee6
Lainarivotiana NIRILANTO <lainarivotiana@gmail.com>
2024-06-01 15:46:33 +0200

    Premier commit

---
+++ MoneyTest.java
@@ -0,0 +1,28 @@
+package tutoteste.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+//import org.junit.Before;
+import org.junit.Test;
+
+import tutoteste.test.Money;
+
+public class MoneyTest {
+
+	@Test
+	public void testSimpleAdd() {
+		Money m12CHF = new Money(12, "CHF"); // création de données 
+		Money m14CHF = new Money(14, "CHF");
+		Money expected = new Money(26, "CHF");
+		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée 
+		assertTrue(result.equals(expected)); // comparaison
+	}	
+	
+	@Before
+	public void testbefore() {
+		System.out.print("Apres");
+	}
+}
+	
+	
